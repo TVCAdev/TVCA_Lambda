e3ce59bb6a8e921f7ef95a6408e3ecdf4c3936fb
TVCA <tvcadev@gmail.com>
2023-09-09 16:46:44 +0900

    add variable at template.yaml

---
+++ lambdas/sns_lambda/db_functions.ts
@@ -0,0 +1,42 @@
+'use strict';
+
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getFirestore, Firestore, WithFieldValue, QueryDocumentSnapshot, Timestamp } from 'firebase-admin/firestore';
+
+/*
+ * Initialize Firebase
+ */
+initializeApp({
+    credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+    }),
+});
+
+export const doc_converter = <T>() => ({
+    toFirestore: (data: WithFieldValue<T>) => data,
+    fromFirestore: (snap: QueryDocumentSnapshot) => snap.data() as T,
+});
+
+export interface stateInRoomTable {
+    inState: boolean;
+}
+
+export interface requestTable {
+    reqDate: Timestamp;
+}
+
+export interface configLocationTable {
+    token: string;
+}
+
+export interface logInroomLogsTable {
+    action: string;
+    date: Timestamp;
+    name: string;
+}
+
+export const db: Firestore = getFirestore();
+
+export default db;
